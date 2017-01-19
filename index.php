1d1a60f307e4e9f461bef6059e1c79c5220ad57a
vgrados <vgrados2@gmail.com>
2017-01-18 22:10:13 -0500

    MIS CAMBIOS

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Document</title>
 </head>
 <body>
-   <h1>VICTOR GRADOS NAUCAPOMA</h1>
+   <h1>VICTOR EDUARDO GRADOS NAUCAPOMA</h1>
+   <h1>PATRICIA GERVACIO MARCELINO</h1>
 </body>
 </html>
