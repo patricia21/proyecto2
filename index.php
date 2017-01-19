de85fb1666591457e9e80aa012ba90ad418d6aa4
Patricia <Patricia>
2017-01-18 22:15:05 -0500

    cambio 1

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-   <h1>VICTOR GRADOS NAUCAPOMA</h1>
-   <h1>patricia gervacio marcelino</h1>
-</body>
-</html>
+<?php
+$x="te";
+$y= "a";
+$z= "m";
+$j= "o";
+
+echo $x." ".$y.$z.$j;
+ ?>
